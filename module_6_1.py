851498722b603fe00391b586f93f3bed61abcf65
Konstantin1983-Fail <kostiieo830@gmail.com>
2024-08-18 01:56:22 +0400

    Update module_6_1.py
    Метод eat должен быть определен только в классе Animal
    Для класса Plant, Fruit атрибут edible должен быть определён на уровне класса, не объект

--- module_6_1.py
+++ module_6_1.py
@@ -1,49 +1,42 @@
 class Animal:
-
     def __init__(self, name):
         self.alive = True
         self.fed = False
         self.name = name
 
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
 
 class Plant:
+    edible = False
 
     def __init__(self, name):
-        self.edible = False
         self.name = name
 
 
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 
 class Flower(Plant):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
 
 
 class Fruit(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-        self.edible = True
+    edible = True
 
 
+# Примеры использования
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
@@ -58,3 +51,7 @@ a1.eat(p1)       # Волк с Уолл-Стрит не стал есть Цве
 a2.eat(p2)       # Хатико съел Заводной апельсин
 print(a1.alive)  # False
 print(a2.fed)    # True
+
+
+
+
